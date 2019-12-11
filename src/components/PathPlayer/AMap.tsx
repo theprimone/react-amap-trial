934126adc973a375ff5328009337b11f9eda7d72
Yuns Xie <yuns.xie@qq.com>
2019-12-11 17:58:16 +0800

    feat: split AMap children

--- src/components/PathPlayer/AMap.tsx
+++ src/components/PathPlayer/AMap.tsx
@@ -1,26 +1,43 @@
 import React, { useState } from 'react';
 import BasicAMap from '../../components/AMap/BasicAMap';
-import { LngLat } from '../Props';
-// import styles from './index.less';
+import { LngLat } from '../props';
+import { injectChildren } from '../../utils';
+import styles from './AMap.less';
 
-export default function ({ height, children }) {
+export interface PathMapProps {
+  panelWidth?: React.CSSProperties['width'];
+  height: React.CSSProperties['height'];
+}
+export default function ({ panelWidth, height, children }: React.PropsWithChildren<PathMapProps>) {
   const [position, setPosition] = useState();
+  const [amapIns, setAmapIns] = useState();
 
   const initCallback = () => {
     console.log("AMapUI Loaded Done.")
   }
 
+  const panelProps = !!panelWidth && {
+    className: styles.panel,
+    style: {
+      width: panelWidth,
+      height,
+    }
+  }
+
   return (
-    <div style={{ height, position: 'relative' }}>
-      <BasicAMap
-        onClick={(lnglat: LngLat) => setPosition({ lng: lnglat.lng, lat: lnglat.lat })}
-        onCreated={(amap) => { amap = amap }}
-        zoom={4}
-        center={position}
-        useAMapUI={initCallback}
-      >
-        {children}
-      </BasicAMap>
+    <div style={{ position: 'relative' }}>
+      <div style={{ height, paddingRight: panelWidth }}>
+        <BasicAMap
+          onClick={(lnglat: LngLat) => setPosition({ lng: lnglat.lng, lat: lnglat.lat })}
+          onCreated={(amap) => { setAmapIns(amap) }}
+          zoom={4}
+          center={position}
+          useAMapUI={initCallback}
+        />
+      </div>
+      <div {...panelProps}>
+        {amapIns && injectChildren(children, { __map__: amapIns })}
+      </div>
     </div>
   )
 }
